7fa041e80d6262830553dd3fb04e50d759d8e814
Yurii Oblovatskyi <yurii.oblovatskyi@check24.de>
2021-11-09 20:25:45 +0100

    fix login page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Page;
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,16 +14,30 @@ class LoginController extends AbstractController
     /**
      * @Route("/login", name="login")
      */
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(AuthenticationUtils $authenticationUtils, PageRepository $repository): Response
     {
 
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        $page = $repository->findOneBy(['main' => true]);
+        if (!$page) {
+            //fallback
+            $page = (new Page())
+                ->setDescription(self::SMTHNG_WRONG)
+                ->setTitle(self::SMTHNG_WRONG)
+                ->setAddition(self::SMTHNG_WRONG)
+                ->setContent1(self::SMTHNG_WRONG)
+                ->setContent2(self::SMTHNG_WRONG)
+                ->setContentTable(self::SMTHNG_WRONG)
+                ->setKeywords(self::SMTHNG_WRONG);
+        }
+
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
+            'page' => $page,
         ]);
     }
 }
